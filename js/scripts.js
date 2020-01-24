97b471a000cc98423ada50b990c9530dbbbffd9c
nellagio <nickgallo41@gmail.com>
2020-01-24 12:54:28 -0800

    work on function, trying to undestand how to do this project...

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
 
     $("#user_number_inpt").submit(function(event) {
-      alert("errrrrrrrror");
-      
-  
+      alert("errrrrrrrror");  
       event.preventDefault();
+
     });
   });
+
+  var beepBoopLogic = function beepBoopLogic(user_number_inpt) {
+
+    var beep = [1]
+    var beep = ["Beep!"]
+    var boop = [2]
+    var boop = ["Boop!"]
+    var imsorry = [3]
+    var imsorry = ["I'm sorry, Dave. I'm afraid I can't do that."]
+  }
