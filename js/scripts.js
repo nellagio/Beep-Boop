abfc3965a7c0307640d413bb55c07591b05a12ea
nellagio <nickgallo41@gmail.com>
2020-01-26 12:57:59 -0800

    for loop, if, else if and else statements complete and functional

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,31 @@ $(document).ready(function() {
     // alert("errrrrrrrror");
     event.preventDefault();
     var numberInputResult = parseInt($("#numberInput").val());
-    $("#numberInputResult").text(numberInputResult);
 
     var listOfNumbers = [];
 
     for (var index = 0; index <= numberInputResult; index += 1) {
       if (index === 3) {
-          listOfNumbers.push("I'm sorry, Dave. I can't do that.");
-        }
-    else {listOfNumbers.push(index);}
+        listOfNumbers.push("I'm sorry, Dave. I can't do that.");
+      }
+      else if (index === 2) {
+        listOfNumbers.push("Boop!");
+      }
+      else if (index === 1) {
+        listOfNumbers.push("Beep!");
+      }
+      else {
+        listOfNumbers.push(index);
+      }
     }
      $("#numberInputResult").text(listOfNumbers);
-      
   });
 });
 
 // business logic
 
-
+function characterTest(number) {
+  var stringTest = number.toString();
+    contains
+}
 
