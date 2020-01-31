662050687514594d6b0b0e5f98bdc881ab245731
nellagio <nickgallo41@gmail.com>
2020-01-31 14:01:05 -0800

    fix loop per travis suggestions. removed my function that i made so i can use a build in function .includes

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,22 @@
 
 $(document).ready(function() {
   $("#spaceForm").submit(function(event) {
-    // alert("errrrrrrrror");
+    alert("errrrrrrrror");
     event.preventDefault();
     var numberInputResult = parseInt($("#numberInput").val());
 
     var listOfNumbers = [];
 
     for (var index = 0; index <= numberInputResult; index += 1) {
-      if (index === 3) {
+      var numberString = index.toString();
+
+      if (numberString.includes("3")) {
         listOfNumbers.push("I'm sorry, Dave. I can't do that.");
       }
-      else if (index === 2) {
+      else if (numberString.includes("2")) {
         listOfNumbers.push("Boop!");
       }
-      else if (index === 1) {
+      else if (numberString.includes("1")) {
         listOfNumbers.push("Beep!");
       }
       else {
@@ -25,11 +27,3 @@ $(document).ready(function() {
      $("#numberInputResult").text(listOfNumbers);
   });
 });
-
-// business logic
-
-function characterTest(number) {
-  var stringTest = number.toString();
-    contains
-}
-
